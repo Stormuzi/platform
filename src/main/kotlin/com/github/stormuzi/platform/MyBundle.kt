caa8c16d52c58576264abedef51fe5b8c828f343
GitHub Action <action@github.com>
2022-01-05 08:59:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/stormuzi/platform/MyBundle.kt
+++ src/main/kotlin/com/github/stormuzi/platform/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.stormuzi.platform
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/stormuzi/platform/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stormuzi/platform/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stormuzi.platform.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stormuzi.platform.MyBundle
 
 class MyProjectService(project: Project) {
 
